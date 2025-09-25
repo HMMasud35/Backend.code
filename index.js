3d83183e0bbf93922d7fef652fe160898762ada4
Hasan Masud <hmmasud635@gmail.com>
2025-09-25 12:25:56 +0600

    route setup and db concction successfull

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+require('dotenv').config()
 const express = require("express")
-
+const dbConnection = require('./src/config/dbconfig')
+const router = require('./src/route')
 const app = express()
+const port = process.env.PORT || 5000
 
+//database connection
+dbConnection()
+
+//router middelware
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`server is running port number ${process.env.PORT}`);
+})
